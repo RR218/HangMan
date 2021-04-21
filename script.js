77c965d904e75a009f350aa3f7f41767f7d6f653
RR218 <81664096+RR218@users.noreply.github.com>
2021-04-21 14:53:59 +0800

    m

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ let answer = "";
 let maxWrong = 6;
 let mistakes = 0;
 let guessed = [];
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 function randWord() {
     answer = randomWords[Math.floor(Math.random() * randomWords.length)];
@@ -27,3 +30,22 @@ function randWord() {
 }
 
 randWord()
+
+
+
+function generateKeyboard() {
+    // create alphabet ul
+    keyboard = document.getElementById('keyboard');
+    letters = document.createElement('ul');
+
+    for (var i = 0; i < alphabet.length; i++) {
+        letters.id = 'alphabet';
+        list = document.createElement('button');
+        list.id = 'letter';
+        list.innerHTML = alphabet[i];
+        keyboard.appendChild(letters);
+        letters.appendChild(list);
+        console.log(keyboard);
+    }
+}
+generateKeyboard();
